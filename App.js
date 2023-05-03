12e7d826489fb3c2d0cd36acd65030898fc7365f
BrunnoDC <brunnodc05@gmail.com>
2023-05-03 10:36:09 -0300

    navigation

--- App.js
+++ App.js
@@ -1,41 +1,40 @@
-import React, { useState } from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Garage from './src/components/Garage';
-import Car from './src/components/Cars';
+import React, { useState } from "react";
+import { StyleSheet, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Garage from "./src/components/Garage";
+import Car from "./src/components/Cars";
 
 const garages = [
   {
     name: "Estacionamento da Bento",
-    image: "https://www.independenciaveiculoseden.com.br/img/pgoogle/capa/full-ec6a965ba159ea5e8e2fda5c8394b7d7.jpg"
-  }
+    image:
+      "https://www.independenciaveiculoseden.com.br/img/pgoogle/capa/full-ec6a965ba159ea5e8e2fda5c8394b7d7.jpg",
+  },
 ];
 
 const cars = [
   {
     carName: "Honda Civic",
     carOwner: "Carlos Cardozo",
-    image: "https://www.pngmart.com/files/4/Honda-Civic-PNG-Photo.png"
-  }
-]
+    image: "https://www.pngmart.com/files/4/Honda-Civic-PNG-Photo.png",
+  },
+];
 
-const ListGarages = () => {
-  const [garageslist, setGaragesList] = useState
-    (garages);
+const ListGarages = ({ navigation }) => {
+  const [garageslist, setGaragesList] = useState(garages);
 
   return (
     <ScrollView style={styles.scroll}>
       {garageslist.map((garage, index) => (
-        <Garage key={index} garage={garage} />
+        <Garage key={index} garage={garage} navigation={navigation} />
       ))}
     </ScrollView>
   );
 };
 
 const ListCars = () => {
-  const [carslist, setCarsList] = useState
-    (cars);
+  const [carslist, setCarsList] = useState(cars);
 
   return (
     <ScrollView style={styles.scroll}>
@@ -43,8 +42,8 @@ const ListCars = () => {
         <Car key={index} car={car} />
       ))}
     </ScrollView>
-  )
-}
+  );
+};
 
 const Stack = createNativeStackNavigator();
 
@@ -63,4 +62,4 @@ const styles = StyleSheet.create({
   scroll: {
     padding: 20,
   },
-})
+});
--- src/components/Garage.js
+++ src/components/Garage.js
@@ -2,48 +2,45 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import { Button } from "react-native-web";
 
-const Garage = ({ garage }) => {
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.text}>{garage.name}</Text>
-            </View>
-            <View style={styles.center}>
-                <Image source={{ uri: garage.image }} style={styles.image} />
-            </View>
-            <Button
-                title="Mais"
-                onPress={() => navigation.navigate('Cars')}
-            />
-        </View>
-    );
+const Garage = ({ garage, navigation }) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>{garage.name}</Text>
+      </View>
+      <View style={styles.center}>
+        <Image source={{ uri: garage.image }} style={styles.image} />
+      </View>
+      <Button title="Mais" onPress={() => navigation.navigate("Cars")} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginBottom: 35,
-        backgroundColor: "#f5f5f5",
-        width: "90%",
-        borderRadius: 30,
-        alignSelf: "center",
-    },
-    center: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: 20,
-    },
-    image: {
-        width: "70%",
-        height: 300,
-        resizeMode: "stretch",
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginBottom: 5,
-        color: "black",
-    },
+  container: {
+    marginBottom: 35,
+    backgroundColor: "#f5f5f5",
+    width: "90%",
+    borderRadius: 30,
+    alignSelf: "center",
+  },
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  image: {
+    width: "70%",
+    height: 300,
+    resizeMode: "stretch",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: "black",
+  },
 });
 
 export default Garage;
