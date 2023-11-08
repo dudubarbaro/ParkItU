f1cc641e092ce5d07462e6c7de297fca0b680885
BrunnoDC <>
2023-11-08 11:36:24 -0300

    mobile rodando

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,90 +1,84 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, StatusBar } from "react-native"
+import { View, Text, Image, StyleSheet, StatusBar } from "react-native";
 import { Button } from "react-native";
 
-import carService from '/src/components/services/cars.js';
-import { useEffect, useState } from 'react';
+import carService from "./services/cars.js";
+import { useEffect, useState } from "react";
 
 export default function Car({ car, navigation }) {
-    const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState([]);
 
-    useEffect(async () => {
-        const data = await carService.getAllCars(car);
-        setCars(data);
-    }, []);
+  useEffect(async () => {
+    const data = await carService.getAllCars(car);
+    setCars(data);
+  }, []);
 
-    async function deleteCars(car) {
-        const data = await carService.getAllCars(car);
-        setCars(data);
-    }
+  async function deleteCars(car) {
+    const data = await carService.getAllCars(car);
+    setCars(data);
+  }
 
-    return (
-        <View>
-            <View style={styles.addButton}>
-                <Button
-                    title="+"
-                    onPress={() => navigation.navigate("Novo Carro")}
-                />
+  return (
+    <View>
+      <View style={styles.addButton}>
+        <Button title="+" onPress={() => navigation.navigate("Novo Carro")} />
+      </View>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          {cars.map((car, index) => (
+            <View style={styles.content} key={index}>
+              <Text style={styles.text}>{car.carName}</Text>
+              <Text style={styles.text2}>{car.carOwner}</Text>
+              <Text style={styles.text2}>{car.licensePlate}</Text>
+              <Text style={styles.text2}>{car.dateTime}</Text>
+              <Button title="Remover" onPress={() => deleteCars()} />
             </View>
-            <View style={styles.container}>
-                <View style={styles.card}>
-                    {cars.map((car, index) => (
-                        <View style={styles.content} key={index}>
-                            <Text style={styles.text}>{car.carName}</Text>
-                            <Text style={styles.text2}>{car.carOwner}</Text>
-                            <Text style={styles.text2}>{car.licensePlate}</Text>
-                            <Text style={styles.text2}>{car.dateTime}</Text>
-                            <Button
-                                title="Remover"
-                                onPress={() => deleteCars()}
-                            />
-                        </View>
-                    ))}
-                </View>
-            </View>
-
-            <StatusBar style="auto" />
+          ))}
         </View>
-    );
-};
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        marginBottom: 35,
-        backgroundColor: "white",
-        width: "100%",
-        borderRadius: 20,
-        alignSelf: "center",
-    },
-    carImage: {
-        width: "70%",
-        height: 200,
-        resizeMode: "stretch",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: 20,
-    },
-    text2: {
-        marginTop: 5,
-        marginLeft: 10,
-        color: "black",
-        height: "100%",
-    },
-    addButton: {
-        marginBottom: 20,
-        width: "30%",
-        alignSelf: "center",
-    },
-    text: {
-        alignSelf: "center",
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    content: {
-        padding: 10,
-    },
-    card: {
-        margin: 30
-    }
+  container: {
+    marginBottom: 35,
+    backgroundColor: "white",
+    width: "100%",
+    borderRadius: 20,
+    alignSelf: "center",
+  },
+  carImage: {
+    width: "70%",
+    height: 200,
+    resizeMode: "stretch",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  text2: {
+    marginTop: 5,
+    marginLeft: 10,
+    color: "black",
+    height: "100%",
+  },
+  addButton: {
+    marginBottom: 20,
+    width: "30%",
+    alignSelf: "center",
+  },
+  text: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  content: {
+    padding: 10,
+  },
+  card: {
+    margin: 30,
+  },
 });
--- src/components/NewCar.js
+++ src/components/NewCar.js
@@ -1,98 +1,101 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity } from "react-native"
-import axios from 'axios';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import axios from "axios";
 // import { Camera, CameraType } from 'expo-camera';
 
 export default function NewCar() {
-    async function postNewCar() {
-        await axios
-            .post("http://127.0.0.1:8000/api/cars/", newCar)
-            .then((response) => console.log(response));
-    }
+  async function postNewCar() {
+    await axios
+      .post("https://backendparkitu-dev.fl0.io/api/cars/", newCar)
+      .then((response) => console.log(response));
+  }
 
-    const [newCar, setNewCar] = useState({})
-    // const [type, setType] = useState(CameraType.back);
-    // const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [newCar, setNewCar] = useState({});
+  // const [type, setType] = useState(CameraType.back);
+  // const [permission, requestPermission] = Camera.useCameraPermissions();
 
-    // function toggleCameraType() {
-    //     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-    // }
+  // function toggleCameraType() {
+  //     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  // }
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.form}>
-                <Text style={styles.h1}
-                >Cadastro</Text>
-                <TextInput
-                    onChangeText={(carName) => setNewCar({ ...newCar, carName })}
-                    style={styles.text}
-                    label={'nome'}
-                    placeholder={'Digite o nome do carro'}
-                ></TextInput>
-                <TextInput
-                    onChangeText={(carOwner) => setNewCar({ ...newCar, carOwner })}
-                    style={styles.text}
-                    label={'dono'}
-                    placeholder={'Digite o nome do dono'}
-                ></TextInput>
-                <TextInput
-                    onChangeText={(licensePlate) => setNewCar({ ...newCar, licensePlate })}
-                    style={styles.text}
-                    label={'placa'}
-                    placeholder={'Digite a placa'}
-                ></TextInput>
-                {/* <Camera style={styles.camera} type={type}>
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.h1}>Cadastro</Text>
+        <TextInput
+          onChangeText={(carName) => setNewCar({ ...newCar, carName })}
+          style={styles.text}
+          label={"nome"}
+          placeholder={"Digite o nome do carro"}
+        ></TextInput>
+        <TextInput
+          onChangeText={(carOwner) => setNewCar({ ...newCar, carOwner })}
+          style={styles.text}
+          label={"dono"}
+          placeholder={"Digite o nome do dono"}
+        ></TextInput>
+        <TextInput
+          onChangeText={(licensePlate) =>
+            setNewCar({ ...newCar, licensePlate })
+          }
+          style={styles.text}
+          label={"placa"}
+          placeholder={"Digite a placa"}
+        ></TextInput>
+        {/* <Camera style={styles.camera} type={type}>
                     <View style={styles.buttonContainer}>
                         <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
                             <Text style={styles.text}>Flip Camera</Text>
                         </TouchableOpacity>
                     </View>
                 </Camera> */}
-                <View>
-                    <Button
-                        title='Enviar'
-                        onPress={() => postNewCar()}
-                    />
-                    <StatusBar style="auto" />
-                </View>
-            </View>
+        <View>
+          <Button title="Enviar" onPress={() => postNewCar()} />
+          <StatusBar style="auto" />
         </View>
-    );
-};
-
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        marginBottom: 35,
-        backgroundColor: "white",
-        width: "100%",
-        borderRadius: 20,
-        alignSelf: "center",
-    },
-    carImage: {
-        width: "70%",
-        height: 200,
-        resizeMode: "stretch",
-        marginBottom: 20,
-    },
-    form: {
-        margin: 45,
-        justifyContent: 'center',
-        alignContent: 'center',
-        backgroundColor: '#fff',
-        justifyContent: 'center'
-    },
-    h1: {
-
-        fontWeight: 'bold',
-        fontSize: 20,
-        padding: 40,
-    },
-    text: {
-        height: 40,
-        margin: 12,
-        padding: 10,
-        borderBottomWidth: 1,
-    },
+  container: {
+    marginBottom: 35,
+    backgroundColor: "white",
+    width: "100%",
+    borderRadius: 20,
+    alignSelf: "center",
+  },
+  carImage: {
+    width: "70%",
+    height: 200,
+    resizeMode: "stretch",
+    marginBottom: 20,
+  },
+  form: {
+    margin: 45,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  h1: {
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 40,
+  },
+  text: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderBottomWidth: 1,
+  },
 });
